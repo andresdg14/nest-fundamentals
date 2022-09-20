3da315f13cf0b8ecd5cc5273f8c3afedf3ac1cf1
Andres Dominguez <andresdomlez@gmail.com>
2022-09-20 11:16:48 +0100

    dtos and configs

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,35 +8,42 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   // Getting a specific coffee by its id
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+  findOne(@Param('id') id: number) {
+    console.log(typeof id);
+    return this.coffeesService.findOne('' + id);
   }
 
   @Post()
-  create(@Body() body) {
-    return body;
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   // Updating a specific coffee
   @Patch(':id')
-  update(id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   // Deleting a specific coffee
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
---
+++ src/coffees/coffees.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
+
+@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+// We have to remove those references from the root module
+export class CoffeesModule {}
